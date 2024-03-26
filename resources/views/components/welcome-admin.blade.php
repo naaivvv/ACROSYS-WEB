48a4d496e870a3c957ca7a482ffb25adbaa3dd06
Kryle8 <151596121+Kryle8@users.noreply.github.com>
2024-03-26 21:11:52 +0800

    admindash

--- resources/views/components/welcome-admin.blade.php
+++ resources/views/components/welcome-admin.blade.php
@@ -5,16 +5,16 @@
 @props(['usersThisMonth'])
 <div class="flex flex-wrap p-6 lg:p-8 w-full">
     <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 p-2">
-        <div class="bg-white border-b border-gray-200 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-clients :totalClients="$totalClients" /></div>
+        <div class="bg-white border border-r-4 border-r-orange-500 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-clients :totalClients="$totalClients" /></div>
     </div>
     <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 p-2">
-        <div class="bg-white border-b border-gray-200 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-organizers :totalOrganizers="$totalOrganizers" /></div>
+        <div class="bg-white border border-r-4 border-r-blue-900 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-organizers :totalOrganizers="$totalOrganizers" /></div>
     </div>
     <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 p-2">
-        <div class="bg-white border-b border-gray-200 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-events :totalEvents="$totalEvents" /></div>
+        <div class="bg-white border border-r-4 border-r-sky-900 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-events :totalEvents="$totalEvents" /></div>
     </div>
     <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 p-2">
-        <div class="bg-white border-b border-gray-200 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-tickets :totalTicketsManaged="$totalTicketsManaged" /></div>
+        <div class="bg-white border border-r-4 border-r-red-600 overflow-hidden shadow-xl sm:rounded-lg min-h-[10rem] max-h-[10rem]"><x-card-tickets :totalTicketsManaged="$totalTicketsManaged" /></div>
     </div>
     <div class="w-full md:w-1/2 lg:w-1/2 p-2 min-h-[40rem] flex flex-col">
         <div class="bg-white border-b border-gray-200 p-6 lg:p-8 overflow-hidden shadow-xl sm:rounded-lg flex justify-center min-h-[24.5rem]">
